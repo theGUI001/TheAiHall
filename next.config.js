4d88d90fe03be4995fa455d34183628519003080
theGUI001 <lghmelo@gmail.com>
2023-01-10 14:08:52 -0300

    remote images add

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'objectstorage.sa-saopaulo-1.oraclecloud.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   experimental: {
     appDir: true,
   },
