ba5cf51b39ddde704d622dade38cf7c93743d865
theGUI001 <lghmelo@gmail.com>
2023-01-13 14:20:14 -0300

    home working

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
-export default async function Home() {
-  return (
-    <div className="w-auto mx-2 my-2 h-full ">
+import styles from './home.module.css'
+import Post from "../Components/Post"
 
-    </div>
+export default async function Home() {
+
+  const src = await fetch(`${process.env.VERCEL_URL || "http://localhost:3000"}/api/posts`)
+  const posts = await src.json()
+
+  return (
+    <div id={styles.plotarea} className="mx-2 my-4 justify-items-center">
+      {posts.map((post, key) => (
+        <Post image={post.url} author={post.author} prompt={post.prompt} key={key} />
+      )
+      )}
+    </div >
   )
 }
