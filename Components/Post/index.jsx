5f887068a889a268c6a0207ec2999899825eb630
theGUI001 <lghmelo@gmail.com>
2023-01-13 14:49:29 -0300

    substring large prompts

--- Components/Post/index.jsx
+++ Components/Post/index.jsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 
 export default function Post({ prompt, image, author }) {
+    let promptN = prompt
+    if (promptN.length > 60) {
+        promptN = promptN.substring(0, 60) + '...';
+    }
     return (
-        <div className=" bg-gray-900 h-full w-72 rounded-xl">
+        <div className=" bg-gray-900 h-full w-72 rounded-xl px-2">
             <div id="img" className="flex justify-center">
                 <Image
                     src={`https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/gr76az1mgnha/b/bucket-aihall/o/${image}`}
@@ -12,7 +16,7 @@ export default function Post({ prompt, image, author }) {
                     className="rounded-2xl p-2 mt-4 mb-1"
                 />
             </div>
-            <h1 className="text-gray-300 text-center mb-1">{`"${prompt}"`}</h1>
+            <h1 className="text-gray-300 text-center mb-1">{`"${promptN}"`}</h1>
             <h1 className="text-white text-center pb-4"><span>by: </span>{author}</h1>
         </div>
     )
