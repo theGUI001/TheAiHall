8f84b5bb1d32d03169c2a8d0f9aab2f99f800a96
theGUI001 <lghmelo@gmail.com>
2023-01-13 09:07:16 -0300

    Discord oAuth removed

--- app/auth/loginerror/page.jsx
+++ app/auth/loginerror/page.jsx
@@ -1,13 +1,10 @@
-import { getProviders } from "next-auth/react"
 import SignInComponent from "../../../Components/SingInComponent/"
 
 async function SignInPage() {
-    const providers = await getProviders()
-
     return (
         <div>
             <h1 className="text-red-600 text-center absolute left-0 right-0 top-36">Invalid Credentials</h1>
-            <SignInComponent providers={providers} />
+            <SignInComponent />
         </div>
     )
 }
--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -1,11 +1,9 @@
-import { getProviders } from "next-auth/react"
 import SignInComponent from "../../../Components/SingInComponent"
 
 async function SignInPage() {
-    const providers = await getProviders()
     return (
         <div>
-            <SignInComponent providers={providers} />
+            <SignInComponent />
         </div>
     )
 }
