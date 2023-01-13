447c53fc868a89375c1434e811ee2ab03eb87e3f
theGUI001 <lghmelo@gmail.com>
2023-01-13 15:17:27 -0300

    change display order

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose()
 
 export default function Posts(req, res) {
     let db = new sqlite3.Database('aihall.db')
-    db.all('SELECT * FROM content', [], (err, rows) => {
+    db.all('SELECT * FROM content order by id DESC', [], (err, rows) => {
         if (err) {
             throw err;
         }
