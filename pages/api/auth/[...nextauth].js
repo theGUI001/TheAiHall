2013a748b8a16c960c1dee4d49260896a79001f0
theGUI001 <lghmelo@gmail.com>
2023-01-14 17:46:51 -0300

    Login working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
+const sqlite3 = require('sqlite3')
+import bcrypt from 'bcrypt'
 
 export const authOptions = {
     // Configure authentication providers
@@ -7,15 +9,31 @@ export const authOptions = {
         CredentialsProvider({
             type: 'credentials',
             credentials: {},
-            authorize(credentials) {
+            async authorize(credentials) {
                 const { email, password } = credentials
 
-                if (email !== process.env.UP_USER || password !== process.env.UP_PASSWORD) {
+                // Connect to db
+                let db = new sqlite3.Database('aihall.db')
+
+                // Create new promisse
+                const result = await new Promise((resolve, reject) => {
+                    // Select provided user and return as promisse response
+                    db.get("SELECT * FROM users WHERE username=?", [email], (err, row) => {
+                        if (err) reject(err)
+                        else resolve(row)
+                    })
+                })
+                // CLose db connection
+                db.close()
+
+                // Verifies if user is valid
+                if (!result) {
                     throw new Error('Invalid Credentials')
                 }
-
-                return { id: '1', name: 'Upload Account' }
-
+                // Verifies if password is correct
+                if (email == result.username && bcrypt.compare(password, result.password)) {
+                    return { id: result.id, user: result.username }
+                }
             }
         })
     ],
--- pages/api/errors.js
+++ pages/api/errors.js
@@ -1,5 +1,5 @@
 export default function Errors(req, res) {
-    if (req.query.error == "Invalid Credentials") {
-        res.redirect("/auth/loginerror", 301)
-    }
+
+    res.redirect("/auth/loginerror", 301)
+
 }
