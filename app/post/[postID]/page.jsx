214deaa4a29b21b88809d8e657b375fe2e1e26c5
theGUI001 <lghmelo@gmail.com>
2023-01-15 03:52:58 -0300

    data fetching fixed

--- app/post/[postID]/page.jsx
+++ app/post/[postID]/page.jsx
@@ -1,15 +1,27 @@
+const sqlite3 = require('sqlite3').verbose()
 import DetailsPost from "../../../Components/DetailsPost"
 
+export const revalidate = 2
+
 async function getData(id) {
-    const src = await fetch(`${process.env.VERCEL_URL || "http://localhost:3000"}/api/post/${id}`, { cache: "force-cache" })
-    const res = await src.json()
-    return res
+    // Connect to db
+    let db = new sqlite3.Database('aihall.db')
+    // Create new promisse
+    const data = new Promise((resolve, reject) => {
+        // Select posts in db
+        db.all(`SELECT * FROM content WHERE ID=?`, [id], (err, rows) => {
+            if (err) reject(err)
+            else resolve(rows.pop())
+        })
+    })
+    // Closes db connection
+    db.close()
+    return await data
 }
 
 export default async function Page({ params }) {
 
     const data = await getData(params.postID)
-
     return <DetailsPost data={data} />
 
 }
