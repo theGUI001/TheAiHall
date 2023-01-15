c6acd382ad96ef061bb28f73df637245185fab89
theGUI001 <lghmelo@gmail.com>
2023-01-15 03:52:54 -0300

    added suspense boundaries + data fetching fixed

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,26 @@
 import styles from './home.module.css'
 import Post from "../Components/Post"
 import Link from 'next/link'
+import { Suspense } from 'react'
+const sqlite3 = require('sqlite3').verbose()
+
+export const revalidate = 2
 
 async function getData() {
-  const src = await fetch(`${process.env.VERCEL_URL || "http://localhost:3000"}/api/posts`, { next: { revalidate: 2 } })
-  return src.json()
+  // Connect to db
+  let db = new sqlite3.Database('aihall.db')
+  // Create new promisse
+  const data = new Promise((resolve, reject) => {
+    // Select posts in db
+    db.all('SELECT * FROM content order by id DESC', [], (err, rows) => {
+      if (err) reject(err)
+      else resolve(rows)
+    })
+  })
+  // Closes db connection
+  db.close()
+  return await data
+
 }
 
 export default async function Home() {
@@ -13,12 +29,14 @@ export default async function Home() {
   return (
     <main>
       <div id={styles.plotarea} className="mx-2 my-4 justify-items-center">
-        {posts.map((post) => (
-          <Link href={`/post/${post.id}#uPost`} key={post.id}>
-            <Post image={post.url} author={post.author} prompt={post.prompt} />
-          </Link>
-        )
-        )}
+        <Suspense fallback={"Loading..."}>
+          {posts.map((post) => (
+            <Link href={`/post/${post.id}`} key={post.id}>
+              <Post image={post.url} author={post.author} prompt={post.prompt} />
+            </Link>
+          )
+          )}
+        </Suspense>
       </div >
     </main>
   )
