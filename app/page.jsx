43761fc2d51bb67b926e0b6f5eb20c06d4a4b244
theGUI001 <lghmelo@gmail.com>
2023-01-15 00:47:14 -0300

    add link to post details

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import styles from './home.module.css'
 import Post from "../Components/Post"
+import Link from 'next/link'
 
 async function getData() {
   const src = await fetch(`${process.env.VERCEL_URL || "http://localhost:3000"}/api/posts`, { next: { revalidate: 2 } })
@@ -7,14 +8,15 @@ async function getData() {
 }
 
 export default async function Home() {
-
   const posts = await getData()
 
   return (
     <main>
       <div id={styles.plotarea} className="mx-2 my-4 justify-items-center">
         {posts.map((post) => (
-          <Post image={post.url} author={post.author} prompt={post.prompt} key={post.id} />
+          <Link href={`/post/${post.id}#uPost`} key={post.id}>
+            <Post image={post.url} author={post.author} prompt={post.prompt} />
+          </Link>
         )
         )}
       </div >
